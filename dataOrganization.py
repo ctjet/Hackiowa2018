5e2f6f0f6385a2cc917f74eb7555a429f84c0982
hoggiesub <hoggiesub1234@gmail.com>
2018-10-06 11:09:25 -0500

    changed it for goodness

--- dataOrganization.py
+++ dataOrganization.py
@@ -5,4 +5,8 @@ Created on Sat Oct  6 10:45:58 2018
 
 from dbfread import DBF
 
-table = DBF('xyz.dbf')
+table = DBF('wwa_201801010000_201812312359.dbf')
+
+for record in table:
+    print (record)
+    
