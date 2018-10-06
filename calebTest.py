862a0ac83939159bd9c140fc70861f11bd65b534
ctjet <cmarting99@gmail.com>
2018-10-06 14:18:01 -0500

    Heatmap!

--- calebTest.py
+++ calebTest.py
@@ -1,4 +1,28 @@
 from dbfread import dbf
 
+"""
+Created on Sat Oct  6 10:19:23 2018
+"""
 
+#Nut sack please work
+import pandas as pd
+import gmplot
+# For improved table display in the notebook
+from IPython.display import display
+
+raw_data = pd.read_csv("C:\\Users\\Caleb\\Desktop\\Projects\\Hackiowa2018\\DB\\IowaCountyData.csv")
+
+# Success! Display the first 5 rows of the dataset
+display(raw_data.head(n=5))
+display(raw_data.info())
+latitudes = raw_data["Lat"]
+longitudes = raw_data["Long"]
+# Creating the location we would like to initialize the focus on. 
+# Parameters: Lattitude, Longitude, Zoom
+gmap = gmplot.GoogleMapPlotter(41.8780, 93.0977, 5)
+
+# Overlay our datapoints onto the map
+gmap.heatmap(latitudes, longitudes)
+
+gmap.draw("my_heatmap.html")
 
