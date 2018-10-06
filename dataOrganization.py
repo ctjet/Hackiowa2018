edc6ea9cc2c88a736634fff8fd1dae522abbc1e0
hoggiesub <hoggiesub1234@gmail.com>
2018-10-06 13:49:25 -0500

    more thing

--- dataOrganization.py
+++ dataOrganization.py
@@ -4,22 +4,32 @@ Created on Sat Oct  6 10:45:58 2018
 """
 
 from dbfread import DBF
+import csv
 
-table = DBF('wwa_201801010000_201812312359.dbf')
+table = DBF('DB/Large/wwa_201801010000_201812312359.dbf')
+
 
 counties = {}
-tempDict = {}
+tempName = ""
 
-countyCounter = 0
+with open('DB/IowaCountyData.csv', 'r') as csv_file:
+    csv_file = csv.reader(csv_file, delimiter=',', quotechar='"')
+    
+    rowCount = 0
+    
+    for row in csv_file:
+        if rowCount != 0:
+            counties[row[0]] = {"County_Name":row[1], "Lat":row[2], "Long":row[3], "numSV":0, "numFF":0}
+        
+        rowCount += 1
 
 for record in table:
     
     if record["NWS_UGC"].startswith("IA"):
-        tempDict["County"] = record["NWS_UGC"]
-        tempDict["Phenom"] = record["PHENOM"]
-        tempDict["Issued"] = record["ISSUED"]
-    
-    counties["warning{}".format(countyCounter)] = tempDict
-    
-    countyCounter = countyCounter + 1
-    
+        
+        if record["PHENOM"] == "SV":
+            counties[record["NWS_UGC"]]["numSV"] += 1
+        elif record["PHENOM"] == "FF":
+            counties[record["NWS_UGC"]]["numFF"] += 1
+
+print(counties)
